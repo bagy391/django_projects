84d92536e42513ae51653d6f2576196dd79724a2
Narasimha Rangan <narasimha.rangan@arcadis.com>
2024-11-05 18:38:49 +0530

    added admin

--- mysite/badminton/admin.py
+++ mysite/badminton/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from badminton.models import Tournament, Match, Team, Player
+
 # Register your models here.
+
+admin.site.register(Tournament)
+admin.site.register(Match)
+admin.site.register(Team)
+admin.site.register(Player)
+
